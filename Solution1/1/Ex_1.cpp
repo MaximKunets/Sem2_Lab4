de4ed2fabd713d4392b5b37b11adc7aee81011c4
Максим Кунец <maxkunets@yandex.ru>
2021-02-25 22:29:55 +0300

    2

---
+++ Solution1/1/Ex_1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef struct Node
+{
+	int value;
+	struct Node* next;
+} Node;
+
+void push_left(Node** head, int value)
+{
+	Node* old_head = (Node*)malloc(sizeof(Node));
+	old_head->value = (**head).value;
+	old_head->next = (**head).next;
+
+	Node* new_head = (Node*)malloc(sizeof(Node));
+	new_head->value = value;
+	new_head->next = old_head;
+	*head = new_head;
+}
+
+void printNode(Node* head)
+{
+	Node* current = head;
+	while (current != NULL)
+	{
+		cout << current->value << " ";
+		current = current->next;
+	}
+}
+
+int main()
+{
+	int value_cin;
+	Node* head = (Node*)malloc(sizeof(Node));
+	cin >> value_cin;
+	head->value = value_cin;
+
+	Node* point_2 = (Node*)malloc(sizeof(Node));
+	cin >> value_cin;
+	point_2->value = value_cin;
+	head->next = point_2;
+
+	Node* point_3 = (Node*)malloc(sizeof(Node));
+	cin >> value_cin;
+	point_3->value = value_cin;
+	point_2->next = point_3;
+
+
+	Node* point_4 = (Node*)malloc(sizeof(Node));
+	point_4 = NULL;
+	point_3->next = point_4;
+
+	int value;
+	cin >> value;
+	push_left(&head, value);
+	cout << "became" << endl;
+	printNode(head);
+}
---
+++ Solution1/Ex_2/Ex_2.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+using namespace std;
+
+struct Node 
+{
+    int value;
+    Node* next;
+};
+
+void fill_list(Node** head) 
+{
+    // creating head of list
+    *head = (Node*)malloc(sizeof(Node));
+    (*head)->next = NULL;
+    (*head)->value = 0;
+
+    // filling rest of list
+    Node* curr = *head;
+    for (int i = 1; i < 5; ++i) {
+        Node* temp = (Node*)malloc(sizeof(Node));
+        curr->next = temp;
+        temp->next = NULL;
+        temp->value = i * i;
+        curr = temp;
+    }
+}
+
+void print_list(Node* head) 
+{
+    for (Node* curr = head; curr != NULL; curr = curr->next) {
+        cout << curr->value << ' ';
+    }
+    cout << endl;
+}
+
+void clear_list(Node** head)
+{
+    
+    
+    Node* curr = (*head)->next;
+    while (curr != NULL)
+    {
+        Node* curr_extra = curr->next;
+        free(curr);
+        curr = curr_extra;
+        
+    }
+    free(curr);
+    *head = NULL;
+}
+
+int main()
+{
+    Node* head = nullptr;
+
+    fill_list(&head);
+
+    print_list(head);
+    cout << endl;
+
+    clear_list(&head);
+
+    print_list(head);
+
+    return 0;
+}
